ef78760f13d8aa5797299ef79c244df903910e32
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-07-08 19:54:21 +0300

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,6 +1,25 @@
-// Use these variables in your code
+const prompt = require('prompt');
+prompt.start();
 
-let s = "This is a cat sitting on a table"
-let c = "t"
 
-// Write code here. 
+prompt.get('input', function (err, result) {
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Number type-a çevirmək üçün parseInt() metodundan istifadə edirik.
+    Vergüllə daxil edilmiş sətri iki fərqli ədədə bölmək üçün split metodundan istifadə edirik.
+    */
+    var input = result.input; // "5,2 4 7 12 9"
+    var input_data = input.split(','); // ['5', '2 4 7 12 9']
+    var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
+    var str = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+    let arr = str.split(" ")
+    let count = 0
+    let arr = s.toLowerCase()
+    for(let i=0; i<arr.length;i++){
+        for(let j=0;j<arr[i].length;j++){
+    if(arr[i][j]==c){
+        count++
+       
+    }}
+    } console.log(count);
+});
